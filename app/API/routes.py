78939f254e86c299ac6626019c327d986b0b3b05
ian <ian@DESKTOP-RCHQUBT.localdomain>
2022-03-29 20:16:05 -0400

    les routes de base avec auth

--- app/API/routes.py
+++ app/API/routes.py
@@ -1,45 +1,216 @@
+
 from flask.wrappers import Request
+from sqlalchemy import delete, false, true
 from app.API import bp
-from app.models import Projet, Contrat
+from app.models import Projet, Contrat, User, Jalon
 from app import app
-from app.config import session, engine
+from app.config import session, engine, Config
 from flask_restful import Resource, Api, abort
 from datetime import datetime, timedelta
-from flask import Response, request
+from flask import Response, redirect, request, url_for
 from flask_apispec.views import MethodResource
 from apispec import APISpec
 from flask_apispec.extension import FlaskApiSpec
 from apispec.ext.marshmallow import MarshmallowPlugin
 from flask_apispec import marshal_with
+import secrets
+import jwt
+from flask_jwt_extended import create_access_token
+from time import time
+from app.API.schemas import ContratSchema, UserSchema, ProjetSchema, JalonSchema
+import json
+import msal
 
-from app.API.schemas import ContratSchema
+
+
+
+appGraph = msal.ConfidentialClientApplication(
+        client_id=Config.client_id, authority=Config.tenant,
+        client_credential=Config.secret)
+
+
 
 api = Api(app)
 
-
-
+def isAutorize(encodeToken):
+    """
+    function to validate token comming from route
+    """   
+    try:
+        user = jwt.decode(encodeToken, Config.SECRET_KEY, algorithms='HS256')        
+        return True
+    except:    
+        return False
+ 
 
 # Serialize the data for the response
-contrat_schema = ContratSchema(many=True)
+contrats_schema = ContratSchema(many=True)
+users_schema = UserSchema(many=True)
+projets_schema = ProjetSchema(many=True)
+jalons_schema = JalonSchema(many=True)
+jalon_schema = JalonSchema()
 
 
-
-class ContratApi(MethodResource,Resource):
-    @marshal_with(contrat_schema)
+class AuthApi(MethodResource,Resource):
+    '''
+    route to log user and return Token
+    '''
     def get(self):
-        "GET all contrat"       
-        contrat = session.query(Contrat).all()
-        return contrat_schema.dump(contrat)
-
-    def post(self):
         
         return
 
+    def post(self):
+        "validate user et return token"
+        data = request.get_json(force=True)
+        
+        user = session.query(User).filter_by(username = data['username']).first()
+        msToken = appGraph.acquire_token_by_username_password('blanchia@ville.valleyfield.qc.ca', password=Config.office_pass, scopes=['https://graph.microsoft.com/.default'])
+        if user:
+            if user.check_password(data['password']):
+                expires = timedelta(days=1)
+                access_token = create_access_token(identity=str(user.id), expires_delta=expires)
+                
+                return ({
+                    'isUser':'true',
+                    'isLogged' : 'true',
+                    'token':access_token,
+                    'msToken': msToken['access_token'],
+                    'message':'successfull login'}, 200)
+            else:
+                return ({
+                'isUser':'true',
+                'isLogged':'false',
+                'token':"",
+                'message':'error'})
 
+        
+        else:
+            return ({
+                'isUser':'false',
+                'isLogged':'false',
+                'token':"",
+                'message':'error'})
+        
+
+api.add_resource(AuthApi, '/api/v1/autorize')
+
+class ContratApi(MethodResource,Resource):
+    @marshal_with(contrats_schema)    
+    def get(self):
+        "GET all contrat"
+        token = request.headers.get('HTTP_AUTHORIZATION')        
+        if isAutorize(token):
+            contrat = session.query(Contrat).all()
+            return contrats_schema.dump(contrat)
+        else:
+            return ({'message':'token not valid or expired'}, 400)
+        
+    
+    def post(self):
+        "Get all contrats"
+        contrat = session.query(Contrat).all()
+        return contrats_schema.dump(contrat)
+        
 
 api.add_resource(ContratApi, '/api/v1/contrat')
 
 
+
+class ProjetApi(MethodResource,Resource):
+    @marshal_with(projets_schema)    
+    def get(self):
+        "GET all projet"
+        token = request.headers.get('HTTP_AUTHORIZATION')        
+        if isAutorize(token):
+            projet = session.query(Projet).all()
+            return projets_schema.dump(projet)
+        else:
+            return ({'message':'token not valid or expired'}, 400)   
+    
+
+api.add_resource(ProjetApi, '/api/v1/projet')
+
+class JalonApi(MethodResource,Resource):
+    @marshal_with(jalons_schema)    
+    def get(self):
+        "GET all jalons"
+        token = request.headers.get('HTTP_AUTHORIZATION')        
+        if isAutorize(token):
+            jalon = session.query(Jalon).all()
+                     
+            return jalons_schema.dump(jalon)
+        else:
+            return ({'message':'token not valid or expired'}, 400)   
+    
+    def post(self):
+        "ADD jalons"
+        token = request.headers.get('HTTP_AUTHORIZATION')        
+        if isAutorize(token):
+            data = request.get_json(force=True)
+                     
+            return jalons_schema.dump(data)
+        else:
+            return ({'message':'token not valid or expired'}, 400) 
+
+api.add_resource(JalonApi, '/api/v1/jalon')
+
+class EditJalonApi(MethodResource,Resource):      
+    def post(self, jalon_id):
+        "edit jalons"
+        token = request.headers.get('HTTP_AUTHORIZATION')        
+        if isAutorize(token):
+            jalon = session.query(Jalon).filter_by(id = jalon_id).first()
+            if request.args.get('etat'):
+                jalon.etat = "complet"
+                session.commit()
+                return jalon_schema.dump(jalon)
+            data = request.get_json(force=True)
+            
+            if not jalon:
+                return ({'message':'there no jalon with this id'}, 400)
+            jalon.date = data.get('date')
+            jalon.commentaire = data.get('commentaire')
+            jalon.etat = data.get('etat')
+            jalon.charge_jalon = data.get('charge_jalon')
+            session.commit()
+            return jalon_schema.dump(jalon)
+        else:
+            return ({'message':'token not valid or expired'}, 400)
+
+    def delete(self, jalon_id) :
+        token = request.headers.get('HTTP_AUTHORIZATION')
+        if isAutorize(token):
+            jalon = session.query(Jalon).filter_by(id = jalon_id).first()
+            session.delete(jalon)
+            session.commit()
+            return {'message':f'jalon {jalon_id} successfull deleted'}
+        else:
+            return ({'message':'token not valid or expired'}, 400)
+
+
+api.add_resource(EditJalonApi, '/api/v1/jalon/<int:jalon_id>')
+
+
+
+
+class UserApi(MethodResource,Resource):
+    @marshal_with(users_schema)    
+    def get(self):
+        "GET all user"
+        token = request.headers.get('HTTP_AUTHORIZATION')        
+        if isAutorize(token):
+            user = session.query(User).all()
+            return users_schema.dump(user) 
+        else:            
+            return ({'message':'token not valid or expired'}, 400)
+        
+
+    def post(self):
+                   
+        return 
+        
+
+api.add_resource(UserApi, '/api/v1/user')
 
 app.config.update({
     'APISPEC_SPEC': APISpec(
@@ -54,3 +225,7 @@ app.config.update({
 docs = FlaskApiSpec(app)
 
 docs.register(ContratApi)
+docs.register(UserApi)
+docs.register(ProjetApi)
+docs.register(JalonApi)
+docs.register(EditJalonApi)
--- app/API/schemas.py
+++ app/API/schemas.py
@@ -1,7 +1,7 @@
-from app.models import Contrat, Projet
+from app.models import Contrat, Projet, User, Jalon
 from flask_marshmallow import Marshmallow
-from marshmallow import post_load, INCLUDE, EXCLUDE, fields
-
+from marshmallow import post_load, INCLUDE, EXCLUDE, fields, Schema
+from marshmallow_sqlalchemy import SQLAlchemySchema, auto_field
 from app import app
 from app.config import session
 
@@ -15,17 +15,71 @@ class ContratSchema(ma.Schema):
         include_fk = True
         unknown = EXCLUDE
         sqla_session = session
-        fields = ('id', 'no', 'desc')
+        fields = ('id', 'no', 'desc', 'projet_id')
         
     @post_load
-    def make_position(self, data, **kwargs):
+    def make_contrat(self, data, **kwargs):
         return Contrat(**data)
 
 
 
+class ProjetSchema(ma.Schema):
+     class Meta:
+        model = Projet
+        include_fk = True
+        unknown = EXCLUDE
+        sqla_session = session
+        fields = ('id', 'no_projet', 'desc', 'cat', 'immo', 'reglA', 'reglB', 'statut', 'affectation', 'prev_courante', 'nature', 'charge')
+     
+     @post_load
+     def make_projet(self, data, **kwargs):
+         return Projet(**data)
+    
+    
 
 
+class UserSchema(Schema):
+    class meta:
+        model = User
+        include_fk = True
+        unknown = EXCLUDE
+        load_instance = True      
+        sqla_session = session        
+
+    id = fields.Integer()
+    username = fields.String()
+    email = fields.String()
+    statut = fields.String()
+    service = fields.String()
+
+    @post_load
+    def make_user(self, data, **kwargs):
+        return User(**data)
 
 
+#utilsation du schema direct de marshmallow pour éviter erreur
+class JalonSchema(Schema):
+    class meta:
+        model = Jalon
+        include_fk = True
+        unknown = EXCLUDE
+        load_instance = True
+        sqla_session = session
+        
+    id = fields.Integer()
+    date = fields.String()
+    jalon = fields.String()
+    commentaire = fields.String()
+    etat = fields.String()
+    charge_jalon = fields.Integer()
+    projet_id =  fields.Integer()
+    contrat_id =  fields.Integer()
 
+
+    @post_load
+    def make_jalon(self, data, **kwargs):
+        return Jalon(**data)
+
+
+        
 
