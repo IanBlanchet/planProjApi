c0011fd0da569467ad461a235317647e4f029e55
ian <ian@DESKTOP-RCHQUBT.localdomain>
2022-07-26 19:59:16 -0400

    enlever admin pas secure

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from app.config import Config
 from app.config import session, Base, engine
-from app.models import Contrat
+from app.models import Contrat, Projet, Reglement, Subvention, Ass_reglement_projet
 import psycopg2
 from flask import Flask
 from flask_cors import CORS
@@ -19,10 +19,13 @@ app = Flask(__name__)
 app.config.from_object(Config)
 CORS(app)
 #jwt = JWTManager(app)
-admin = Admin(app, name='contratGref', template_mode='bootstrap4')
-admin.add_view(ModelView(Contrat, session))
-
-
+admin = Admin(app, name='planProjet', template_mode='bootstrap4')
+#utilisation au besoin;
+#admin.add_view(ModelView(Contrat, session))
+#admin.add_view(ModelView(Projet, session))
+#admin.add_view(ModelView(Reglement, session))
+#admin.add_view(ModelView(Subvention, session))
+#admin.add_view(ModelView(Ass_reglement_projet, session))
 
 from app.API import bp as API_bp
 app.register_blueprint(API_bp)
